730996384ae5c95062c528eb903823404687f374
nicol <nicol@DESKTOP-1V2LOQ9>
2022-09-14 13:05:22 -0300

    Agregar archivos de proyecto.

---
+++ Info++/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Info__
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtUnidades = new System.Windows.Forms.TextBox();
+            this.txtProducto = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtSIVASD = new System.Windows.Forms.TextBox();
+            this.txtCIVASD = new System.Windows.Forms.TextBox();
+            this.txtSIVACD = new System.Windows.Forms.TextBox();
+            this.txtCIVACD = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtUnidadesR = new System.Windows.Forms.TextBox();
+            this.txtProductoR = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Info__.Properties.Resources.xper;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtDescuento);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.txtUnidades);
+            this.groupBox1.Controls.Add(this.txtProducto);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.BackColor = System.Drawing.Color.Snow;
+            this.txtDescuento.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtDescuento, "txtDescuento");
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.TextChanged += new System.EventHandler(this.txtDescuento_TextChanged);
+            this.txtDescuento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDescuento_KeyPress);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.BackColor = System.Drawing.Color.Snow;
+            this.txtPrecio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtPrecio, "txtPrecio");
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // txtUnidades
+            // 
+            this.txtUnidades.BackColor = System.Drawing.Color.Snow;
+            this.txtUnidades.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtUnidades, "txtUnidades");
+            this.txtUnidades.Name = "txtUnidades";
+            this.txtUnidades.TextChanged += new System.EventHandler(this.txtUnidades_TextChanged);
+            this.txtUnidades.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUnidades_KeyPress);
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.BackColor = System.Drawing.Color.Snow;
+            this.txtProducto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtProducto, "txtProducto");
+            this.txtProducto.Name = "txtProducto";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.txtSIVASD);
+            this.groupBox2.Controls.Add(this.txtCIVASD);
+            this.groupBox2.Controls.Add(this.txtSIVACD);
+            this.groupBox2.Controls.Add(this.txtCIVACD);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.txtUnidadesR);
+            this.groupBox2.Controls.Add(this.txtProductoR);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // txtSIVASD
+            // 
+            this.txtSIVASD.BackColor = System.Drawing.Color.Snow;
+            this.txtSIVASD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtSIVASD, "txtSIVASD");
+            this.txtSIVASD.Name = "txtSIVASD";
+            this.txtSIVASD.ReadOnly = true;
+            // 
+            // txtCIVASD
+            // 
+            this.txtCIVASD.BackColor = System.Drawing.Color.Snow;
+            this.txtCIVASD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtCIVASD, "txtCIVASD");
+            this.txtCIVASD.Name = "txtCIVASD";
+            this.txtCIVASD.ReadOnly = true;
+            // 
+            // txtSIVACD
+            // 
+            this.txtSIVACD.BackColor = System.Drawing.Color.Snow;
+            this.txtSIVACD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtSIVACD, "txtSIVACD");
+            this.txtSIVACD.Name = "txtSIVACD";
+            this.txtSIVACD.ReadOnly = true;
+            // 
+            // txtCIVACD
+            // 
+            this.txtCIVACD.BackColor = System.Drawing.Color.Snow;
+            this.txtCIVACD.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtCIVACD, "txtCIVACD");
+            this.txtCIVACD.Name = "txtCIVACD";
+            this.txtCIVACD.ReadOnly = true;
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // txtUnidadesR
+            // 
+            this.txtUnidadesR.BackColor = System.Drawing.Color.Snow;
+            this.txtUnidadesR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.txtUnidadesR, "txtUnidadesR");
+            this.txtUnidadesR.Name = "txtUnidadesR";
+            this.txtUnidadesR.ReadOnly = true;
+            // 
+            // txtProductoR
+            // 
+            this.txtProductoR.BackColor = System.Drawing.Color.Snow;
+            this.txtProductoR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtProductoR.Cursor = System.Windows.Forms.Cursors.IBeam;
+            resources.ApplyResources(this.txtProductoR, "txtProductoR");
+            this.txtProductoR.Name = "txtProductoR";
+            this.txtProductoR.ReadOnly = true;
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // btnCalcular
+            // 
+            resources.ApplyResources(this.btnCalcular, "btnCalcular");
+            this.btnCalcular.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtProducto;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtUnidades;
+        private System.Windows.Forms.TextBox txtSIVASD;
+        private System.Windows.Forms.TextBox txtCIVASD;
+        private System.Windows.Forms.TextBox txtSIVACD;
+        private System.Windows.Forms.TextBox txtCIVACD;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtUnidadesR;
+        private System.Windows.Forms.TextBox txtProductoR;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Info++/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Info__
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUnidades_KeyPress(object sender, KeyPressEventArgs e)
+        {
+         
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void txtDescuento_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDescuento_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string Producto;
+            int Unidades;
+            double Precio, Descuento, subtotal, subtotalCD, subtotalCDIVA, subtotalIVA, IVA;
+
+
+            if (txtProducto.Text == "")
+            {
+                MessageBox.Show("Ingresar Datos Alfabeticos");
+
+                return;
+            }
+            if (txtUnidades.Text == "" || txtPrecio.Text == "" || txtDescuento.Text == "")
+            {
+                MessageBox.Show("Ingresar Datos Numericos");
+
+                return;
+            }
+            
+            Producto = txtProducto.Text;
+            Unidades = int.Parse(txtUnidades.Text);
+            Precio = Convert.ToDouble(txtPrecio.Text);
+            Descuento = Convert.ToDouble(txtDescuento.Text);
+
+            subtotal = Precio * Unidades;
+            subtotalCD = subtotal - (subtotal * (Descuento / 100));
+            IVA = 0.21;
+            subtotalCDIVA = subtotalCD + subtotalCD * IVA;
+            subtotalIVA = subtotal + subtotal * IVA;
+                     
+
+            txtProductoR.Text = Producto.ToString();
+            txtUnidadesR.Text = Unidades.ToString();
+            txtCIVACD.Text = "$" + subtotalCDIVA.ToString();
+            txtCIVASD.Text = "$" + subtotalIVA.ToString();
+            txtSIVACD.Text = "$" + subtotalCD.ToString();
+            txtSIVASD.Text = "$" + subtotal.ToString();
+        }
+
+        private void txtUnidades_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
